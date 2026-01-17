ef1db78eddff4c8704449aaf418e3f6ac5ef67ea
Vlados <vladgrigorea@gmail.com>
2026-01-17 15:41:14 +0300

    фигня коммит

--- backend/main.py
+++ backend/main.py
@@ -154,7 +154,11 @@ async def health_check():
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://127.0.0.1:5500", "http://localhost:3002"],
+    allow_origins=[
+        "http://127.0.0.1:5500",
+        "http://localhost:3002",
+        #"http://194.156.117.39:3002"
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
